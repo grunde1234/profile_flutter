962b069bf0577b39437848950951d80e4fb49a2b
Okafor Ebuka <oebuka198@gmail.com>
2025-04-02 19:31:25 +0100

    clipboard added

--- lib/main.dart
+++ lib/main.dart
@@ -62,97 +62,105 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 70.0),
-              CircleAvatar(
-                backgroundImage: AssetImage('image/user.png'),
-                radius: 50.0,
-                backgroundColor: Colors.red[50],
-              ),
-              Text(
-                'GRUND.EO',
-                style: TextStyle(
-                  fontFamily: 'FugazOne',
-                  color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
+      body: Center(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('image/user.png'),
+                  radius: 50.0,
+                  backgroundColor: Colors.red[50],
                 ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  letterSpacing: 5.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(color: Colors.teal.shade100),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: Icon(Icons.phone, color: Colors.teal),
-                    title: Text(
-                      '+234 9012798330',
-                      style: TextStyle(
-                        color: Colors.teal,
-                        fontFamily: 'Roboto',
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    trailing: Icon(
-                      _copiedPhone ? Icons.check : Icons.copy,
-                      color: Colors.teal,
-                    ),
-                    onTap:
-                        () => copyToClipboard(context, '+234 9012798330', true),
+                Text(
+                  'GRUND.EO',
+                  style: TextStyle(
+                    fontFamily: 'FugazOne',
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: Icon(Icons.mail, color: Colors.teal),
-                    title: Text(
-                      'oebuka198@gmail.com',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Roboto',
-                        color: Colors.teal,
-                      ),
-                    ),
-                    trailing: Icon(
-                      _copiedEmail ? Icons.check : Icons.copy,
-                      color: Colors.teal,
-                    ),
-                    onTap:
-                        () => copyToClipboard(
-                          context,
-                          'oebuka198@gmail.com',
-                          false,
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    letterSpacing: 5.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(color: Colors.teal.shade100),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 15.0,
+                    vertical: 10.0,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: Icon(Icons.phone, color: Colors.teal),
+                      title: Text(
+                        '+234 9012798330',
+                        style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'Roboto',
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
                         ),
+                      ),
+                      trailing: Icon(
+                        _copiedPhone ? Icons.check : Icons.copy,
+                        color: Colors.teal,
+                      ),
+                      onTap:
+                          () =>
+                              copyToClipboard(context, '+234 9012798330', true),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 15.0,
+                    vertical: 10.0,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: Icon(Icons.mail, color: Colors.teal),
+                      title: Text(
+                        'oebuka198@gmail.com',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Roboto',
+                          color: Colors.teal,
+                        ),
+                      ),
+                      trailing: Icon(
+                        _copiedEmail ? Icons.check : Icons.copy,
+                        color: Colors.teal,
+                      ),
+                      onTap:
+                          () => copyToClipboard(
+                            context,
+                            'oebuka198@gmail.com',
+                            false,
+                          ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
